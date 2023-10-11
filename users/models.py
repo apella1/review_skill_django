6b8abb53f0320b5a5fdf75fbd1ccc0aedf93e835
apella1 <johncollons5@gmail.com>
2023-10-11 09:28:36 +0300

    Add user model

---
+++ users/models.py
@@ -0,0 +1,15 @@
+import uuid
+
+from django.db import models
+
+
+class User(models.Model):
+    objects = models.Manager()
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+    bio = models.TextField()
+
+    def __str__(self) -> str:
+        return str(f"{self.first_name} {self.last_name}")
